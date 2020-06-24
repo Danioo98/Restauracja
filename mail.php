26dd40d9d11e5a722b03d529dd75d006d1cb6797
Danioo98 <64258713+Danioo98@users.noreply.github.com>
2020-06-24 16:12:23 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,53 +1,14 @@
 <?php
 
-$mailToSend = "twoj-mail@lorem.pl";
+$to      = 'example@example.com';
+$name    = $_POST['name'];
+$email   = $_POST['email'];
+$subject = 'Nowy e-mail od ' . $name . ' ( ' . $email . ')';
+$message = $_POST['message'];
+$headers = 'From: ' . $name . ' (' . $email . ')';
+$headers .= 'Content-Type: text/html; charset=utf-8\r\n';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+mail($to, $subject, $message, $headers);
 
-    $errors = [];
-	$return = [];
-
-    if (empty($name)) { 
-        array_push($errors, "name");
-    }
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
-        array_push($errors, "email");
-    }
-    if (empty($message)) {
-        array_push($errors, "message");
-    }
-
-    if (count($errors) > 0) {
-        $return["errors"] = $errors;
-    } else {
-        
-        $headers  = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type: text/html; charset=UTF-8". "\r\n";
-        $headers .= "From: ".$email."\r\n";
-        $headers .= "Reply-to: ".$email;
-        $message  = "
-            <html>
-                <head>
-                    <meta charset=\"utf-8\">
-                </head>
-                <body>
-                    <div> Imię: $name</div>
-                    <div> Email: <a href=\"mailto:$email\">$email</a> </div>
-                    <div> Wiadomość: </div>
-                    <div> $message </div>
-                </body>
-            </html>";
-
-        if (mail($mailToSend, "Wiadomość ze strony - " . date("d-m-Y"), $message, $headers)) {
-            $return["status"] = "ok";
-        } else {
-            $return["status"] = "error";
-        }
-    }
-
-    header("Content-Type: application/json");
-    echo json_encode($return);
-}
+echo '<h1>Wiadomość wysłana :)</h1>;
+?>
