5d9b642c03494123717770b75012331e271b4608
Danioo98 <64258713+Danioo98@users.noreply.github.com>
2020-07-06 12:22:51 +0200

    Add files via upload

---
+++ kontakt.php
@@ -0,0 +1,13 @@
+<?
+
+$to = 'example@example.com';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = 'Nowy e-mail od ' . $name . ' ( ' . $email . ')';
+$message = $_POST['message'];
+$headers = 'From: ' . $name . ' ( ' .$email . ')';
+$headers = "Content-Type: text/html; charset=utf-8\r\n";
+
+mail($to, $subject, $message, $headers);
+
+?>
